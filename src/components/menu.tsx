6c12d7be6cee8afe635dd8df8942453853d9c618
Everaldo-Martins <everaldoinfortecnico@gmail.com>
2024-07-17 16:20:34 -0300

    Update menu mobile

--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -1,42 +1,43 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from '../assets/logo.svg';
 
 const Menu: React.FC = () => {
-    useEffect(() => {
-        const menuBtn = document.querySelector('.menu-btn');
-        const menu = document.querySelector('.menu');
+  const [isOpen, setIsOpen] = useState(false);
 
-        if (menuBtn && menu) {
-            menuBtn.addEventListener('click', () => {
-                menu.classList.toggle('active');
-            });
-            
-            return () => {
-                menuBtn.addEventListener('click', () => {
-                    menu.classList.toggle('active');
-                });
-            }
-        }
-    }, []);
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth > 768) {
+        setIsOpen(true);
+      }
+    };
 
-    return (
-        <nav className="navbar">
-            <div className="max-width">
-                <div className="logo">
-                    <a href="./"><img src={logo} alt="EMD" /></a>
-                </div>
-                <ul className="menu">
-                    <li><a href="#home">Home</a></li>
-                    <li><a href="#about">Sobre</a></li>
-                    <li><a href="#experience">Experiência</a></li>
-                    <li><a href="#skills">Habilidades</a></li>
-                    <li><a href="#teams">Equipe</a></li>
-                    <li><a href="#contact">Contato</a></li>
-                </ul>
-                <div className="menu-btn fas fa-bars"></div>
-            </div>
-        </nav>
-    );
+    window.addEventListener('resize', handleResize);
+
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <nav className="navbar">
+      <div className="max-width">
+        <div className="logo">
+          <a href="./"><img src={logo} alt="EMD" /></a>
+        </div>
+        <ul className={`menu ${isOpen ? 'active' : ''}`}> {}
+          <li><a href="#home">Home</a></li>
+          <li><a href="#about">Sobre</a></li>
+          <li><a href="#experience">Experiência</a></li>
+          <li><a href="#skills">Habilidades</a></li>
+          <li><a href="#teams">Equipe</a></li>
+          <li><a href="#contact">Contato</a></li>
+        </ul>
+        <div className="menu-btn fas fa-bars" onClick={toggleMenu} />
+      </div>
+    </nav>
+  );
 };
 
 export default Menu;
