7717067513003c02b7bc2480772fdadb44f97cdc
Everaldo-Martins <everaldoinfortecnico@gmail.com>
2025-02-17 07:49:29 -0300

    Upgrade

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -3,8 +3,17 @@ import { Link } from 'react-router-dom';
 import Typewriter from '../components/Typewriter';
 import User from '../assets/images/user.png';
 import '../style/Home.css';
+import '../style/BounceCSS.css';
 
-const Home: React.FC = () => {      
+const Home: React.FC = () => {   
+    const nbols = 30;
+    const renderbols = () => {
+        const bols = [];
+        for (let i = 1; i <= nbols; i++) {
+            bols.push(<span key={i} className={`bol bol-${i}`}></span>);
+        }
+        return bols;
+    };    
     return (       
         <section className="home">
             <div className="box-content">
@@ -34,7 +43,8 @@ const Home: React.FC = () => {
                 <div className="home-right">
                     <figure>
                         <img src={User} alt="" />                        
-                    </figure>                 
+                    </figure>   
+                    {renderbols()}               
                 </div>
             </div>
         </section>
